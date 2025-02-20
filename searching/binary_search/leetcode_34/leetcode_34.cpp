8d907702ed02eab13d8fd43be5ee01e8061b1027
minhdieuvu08 <vudieuminh1973@gmail.com>
2025-02-20 15:19:18 +0700

    Update leetcode_34.cpp

--- searching/binary_search/leetcode_34/leetcode_34.cpp
+++ searching/binary_search/leetcode_34/leetcode_34.cpp
@@ -4,12 +4,26 @@ using namespace std;
 class Solution {
 private:
     int lowerBound(vector<int>& nums, int target) {
+        // We will define two variables:
+        // 'low' starts at the first position in the array.
+        // 'high' starts at the last position in the array.
         int low = 0, high = nums.size() - 1;
+
         while (low <= high) {
+            // Compute the middle position.
+            // We use this formula instead of (high + low) / 2 to prevent integer overflow.
             int mid = low + (high - low) / 2;
+
+            // If the number at the middle position is not the target, we have two cases:
+            // Case 1: The number at the middle position is smaller than the target.
             if (nums[mid] < target) {
+                // The target must be in the range [mid + 1, high].
+                // So we update 'low' to 'mid + 1'.
                 low = mid + 1;
             } else {
+                // Case 2: The number at the middle position is greater than or equal to the target.
+                // The target must be in the range [low, mid-1]
+                // So we move 'high' to 'mid - 1' to search in the left half.
                 high = mid - 1;
             }
         }
@@ -21,9 +35,11 @@ public:
         int start = lowerBound(nums, target);
         int end = lowerBound(nums, target + 1) - 1;
 
+        // If the target exists in the array, return its range.
         if (start < nums.size() && nums[start] == target) {
             return {start, end};
         }
+        // Otherwise, return [-1, -1] to indicate that the target is not found.
         return {-1, -1};
     }
 };
